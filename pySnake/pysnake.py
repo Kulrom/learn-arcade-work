f5e7285c0a8df8bf70ccb7c79cc848337e756412
Роман Кулаков <kulromgame@gmail.com>
2021-12-13 08:14:56 +0300

    Небольшая приборка в коде по PEP8

--- pySnake/pysnake.py
+++ pySnake/pysnake.py
@@ -1,16 +1,16 @@
 import arcade
+from typing import Optional
 
 CELL_SIZE = 20
 GAME_SIZE = (40, 40)
 WIDTH = CELL_SIZE * GAME_SIZE[0]
 HEIGHT = CELL_SIZE * GAME_SIZE[1] + 20
-print(HEIGHT)
 TITLE = 'pySnake'
 BG_COLOR = arcade.color.WHITE_SMOKE
 SNAKE_PART_COLOR = arcade.color.GO_GREEN
 SNAKE_HEAD_COLOR = arcade.color.GRAPE
 START_LENGTH = 6  # Начальная длина питона
-START_DELAY = 1/5 # Начальная задержка таймера
+START_DELAY = 1/5  # Начальная задержка таймера
 
 
 class Timer:
@@ -40,7 +40,7 @@ class SnakePart:
     def draw(self):
         x = self.x * CELL_SIZE + CELL_SIZE/2
         y = self.y * CELL_SIZE + CELL_SIZE/2
-        arcade.draw_circle_filled(x, y, CELL_SIZE / 2 , self.color)
+        arcade.draw_circle_filled(x, y, CELL_SIZE / 2, self.color)
 
     def __str__(self):
         return f'x={self.x}  y = {self.y}'
@@ -51,7 +51,7 @@ class SnakeHead(SnakePart):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.color = SNAKE_HEAD_COLOR
-        self.move = 'right'  # Может быть 'left', 'right', 'up', 'dowm'
+        self.move = 'right'  # Может быть 'left', 'right', 'up', 'down'
         self._dx = 0
         self._dy = 0
 
@@ -75,12 +75,13 @@ class SnakeHead(SnakePart):
 class Snake:
     """Класс описывающий всего питона целиком"""
     def __init__(self, x, y):
+        self.is_game_over = False
         self.part_list = []
         self.length = START_LENGTH
         self.head = SnakeHead(x, y)
         self.part_list.append(self.head)
         for i in range(1, START_LENGTH):
-            snake_part = SnakePart(x-i , y)
+            snake_part = SnakePart(x-i, y)
             self.part_list.append(snake_part)
 
     def _make_step(self):
@@ -96,11 +97,8 @@ class Snake:
             part.x += dx
             part.y += dy
 
-    def change_move(self, way: str):
-        self.head.change_move(way)
-
-    def update(self):
-        self._make_step()
+    def _check_borders(self):
+        """Проверяет выход за границы экрана"""
         if self.head.x < 0:
             self.head.x = GAME_SIZE[0]
         elif self.head.x > GAME_SIZE[0] - 1:
@@ -110,14 +108,25 @@ class Snake:
         elif self.head.y > GAME_SIZE[1] - 1:
             self.head.y = 0
 
+    def _check_game_over(self):
+        x = self.head.x
+        y = self.head.y
+        for part in self.part_list:
+            if part.x == x and part.y == y:
+                self.is_game_over = True
+                break
+
+    def change_move(self, way: str):
+        self.head.change_move(way)
+
+    def update(self):
+        self._make_step()  # Делаем шаг
+        self._check_borders()  # Проверяем выход за границы экрана
     
     def draw(self):
         # print(self.part_list[0], self.part_list[1])
         for part in self.part_list:
             part.draw()
-        text = f'x = {self.head.x}, y = {self.head.y}'
-        arcade.draw_text(text, 0, 800, color=arcade.color.BLACK)
-
 
 
 class MyGame(arcade.Window):
@@ -125,6 +134,7 @@ class MyGame(arcade.Window):
         super().__init__(WIDTH, HEIGHT, TITLE, update_rate=1/60)
         self.background_color = BG_COLOR
         self.timer = Timer(start_delay=START_DELAY)
+        self.snake: Optional[Snake] = None
         self.setup()
 
     def setup(self):
@@ -148,6 +158,8 @@ class MyGame(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.snake.draw()
+        text = f'x = {self.snake.head.x}, y = {self.snake.head.y}'
+        arcade.draw_text(text, 0, 800, color=arcade.color.BLACK)
 
 
 if __name__ == '__main__':
